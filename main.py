6666a55a31fa4df46174c0f16dfe1c3bb8fa95cd
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-05-31 13:37:34 +0300

    ex 4

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-#3
-class ListIterator:
-    def __init__(self, lst):
-        self.data = lst
-        self.index = 0
-    def __iter__(self):
-        return self
-    def __next__(self):
-        if self.index >= len(self.data):
-            raise StopIteration
-        value = self.data[self.index]
-        self.index += 1
-        return value
-my_list = [1,2,3,4,5,6,7,8,9,10]
-my_iter = ListIterator(my_list)
-for num in my_list:
-    print(num)
+#4
+def multiply_by(n):
+    def multiply(x):
+        return x * n
+    return multiply
+multiply_by_5 = multiply_by(3)
+result = multiply_by_5(10)
+print(result)
+
+multiply_by_3 = multiply_by(3)
+result = multiply_by_3(5)
+print(result)

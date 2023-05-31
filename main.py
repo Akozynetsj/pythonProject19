d925cae13c1dace4f6a4ee9ed1bc45221633494b
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-05-31 13:33:08 +0300

    ex 1

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-
+#1
+class OddIterator:
+    def __init__(self, name):
+        self.name = name
+        self.index = 0
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.index >= len(self.name):
+            raise StopIteration
+        if self.index < 0:
+            raise ValueError("Помилка!")
+        value = self.name[self.index]
+        self.index += 2
+        return value
+my_list = [1,2,3,4,5,6,7,8,9,10]
+my_iter = OddIterator(my_list)
+for num in my_iter:
+    print(num)
